5c1c09b3c24fddf0b69200c3fd0e4dc887df95bc
William Cotton <williamcotton@gmail.com>
2015-10-08 11:12:06 -0700

    v1.0.0 - parse input scriptsig for address

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
-var bitcoin = require('bitcoinjs-lib');
+var bitcoin = require('bitcoinjs-lib')
+var bs58check = require('bs58check')
 
-var txHexToJSON = function(hex) {
-  var tx = bitcoin.Transaction.fromHex(hex);
-  var txid = tx.getId();
-  var vin = [];
-  tx.ins.forEach(function(input) {
-    var input_txid = bitcoin.bufferutils.reverse(input.hash).toString("hex");
+var txHexToJSON = function (hex) {
+  var tx = bitcoin.Transaction.fromHex(hex)
+  var txid = tx.getId()
+  var vin = []
+  tx.ins.forEach(function (input) {
+    var input_txid = bitcoin.bufferutils.reverse(input.hash).toString('hex')
+    var input_hex = ''
+    var input_addresses = []
+    if (input.script.chunks.length === 2) {
+      var pubKey = input.script.chunks[1]
+      var pubKeyHash = bitcoin.crypto.hash160(pubKey)
+      var payload = new Buffer(21)
+      payload.writeUInt8(bitcoin.networks.testnet.pubKeyHash, 0)
+      pubKeyHash.copy(payload, 1)
+      var address = bs58check.encode(payload)
+      input_addresses = [address]
+      input_hex = input.script.buffer.toString('hex')
+    }
     vin.push({
       txid: input_txid,
       txId: input_txid,
       vout: input.index,
       scriptSig: {
-        hex: input.script.buffer.toString("hex")
+        hex: input_hex
       },
       sequence: input.sequence,
-      addresses: ["msLoJikUfxbc2U5UhRSjc2svusBSqMdqxZ"]
-    });
-  });
-  var vout = [];
-  tx.outs.forEach(function(output, index) {
-    var script_type = bitcoin.scripts.classifyOutput(output.script);
-    var address = script_type == "pubkeyhash" || script_type == "scripthash" ? bitcoin.Address.fromOutputScript(output.script, bitcoin.networks.testnet).toString() : null;
+      addresses: input_addresses
+    })
+  })
+  var vout = []
+  tx.outs.forEach(function (output, index) {
+    var script_type = bitcoin.scripts.classifyOutput(output.script)
+    var address = script_type === 'pubkeyhash' || script_type === 'scripthash' ? bitcoin.Address.fromOutputScript(output.script, bitcoin.networks.testnet).toString() : null
     vout.push({
       value: output.value,
       index: index,
       n: index,
       scriptPubKey: {
-        hex: output.script.buffer.toString("hex"),
+        hex: output.script.buffer.toString('hex'),
         asm: output.script.toASM(),
         type: script_type,
         addresses: [address]
       }
-    });
-  });
+    })
+  })
   return {
     confirmations: null,
     blockheight: null,
@@ -48,6 +61,6 @@ var txHexToJSON = function(hex) {
     vin: vin,
     vout: vout
   }
-};
+}
 
-module.exports = txHexToJSON;
+module.exports = txHexToJSON
